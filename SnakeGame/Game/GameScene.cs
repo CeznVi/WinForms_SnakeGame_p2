8b362ae66df0a067f3d8b8f0a7f71a2d04f28516
Viktor Velychko <Cezn.Vi@gmail.com>
2023-03-18 19:05:59 +0200

    WIP

---
+++ SnakeGame/Game/GameScene.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame.Game
+{
+    static public class GameScene
+    {
+        
+        public static void Start()
+        {
+            MessageBox.Show("Для начала игры нажмите старт",
+                "Уведомление",
+                MessageBoxButtons.OK
+                );
+
+           
+
+
+        }
+
+    }
+}
