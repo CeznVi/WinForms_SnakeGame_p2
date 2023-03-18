cbd7bb07eca8b1c2149736f8a0bda46a8b32aadd
Viktor Velychko <Cezn.Vi@gmail.com>
2023-03-19 01:56:39 +0200

    WIP

--- SnakeGame/MainForm.Designer.cs
+++ SnakeGame/MainForm.Designer.cs
@@ -234,7 +234,7 @@
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip;
             this.MaximizeBox = false;
